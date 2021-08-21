581cc1248f0310f51e83991317759f0cace78a82
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-21 14:12:11 +0530

    feat:Welcome to Starbucks Coffee Maker

---
+++ CoffeeMaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoffeeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to StarBucks Coffee Maker!");
+        }
+    }
+}
---
+++ CoffeeMaker/StarBucksStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeMaker
+{
+    public class StarbuckStore
+    {
+        private readonly IMakeACoffee service;
+
+        public StarbuckStore(IMakeACoffee service)
+        {
+            this.service = service;
+        }
+
+        public string OrderCoffee(int sugarPerSpoon, int coffeeCount)
+        {
+            if (service.CheckIngredientAvailability())
+            {
+                return service.CoffeeMaking(sugarPerSpoon, coffeeCount);
+            }
+            else
+            {
+                return "Sorry! Coffee is not available.";
+            }
+        }
+
+    }
+
+}
